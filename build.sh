a9399d7159df249948a5a83f6f9f6386d32911fe
Vlad Skvortsov <vss@73rus.com>
2017-11-04 10:53:13 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#! /bin/sh -e
+
+docker build -t mini-mailgun/frontend -f frontend.df .
---
+++ frontend.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python3
+
+import http.server
+import json
+import queue
+import socketserver
+import threading
+
+incoming_queue = queue.Queue(10)
+
+class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
+    pass
+
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/logs':
+            try:
+                body = self._get_json_body()
+            except ValueError as e:
+                self.send_error(400, e.args[0])
+                return
+
+            incoming_queue.put(body)
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write('{}\n'.format(json.dumps({'status': 'success'})).encode())
+        else:
+            self.send_error(404, 'Not found')
+
+    def _get_json_body(self):
+        if self.headers['Content-Type'] != 'application/json':
+            raise ValueError('Expected application/json data')
+
+        try:
+            content_length = int(self.headers['Content-Length'])
+        except ValueError:
+            raise ValueError('Invalid content length')
+
+        return json.loads(self.rfile.read(content_length).decode())
+
+
+class MailQueueAppender(threading.Thread):
+    def __init__(self):
+        super(MailQueueAppender, self).__init__()
+        self.daemon = True
+
+    def run(self):
+        while True:
+            message = incoming_queue.get()
+
+
+def main():
+    mailq_appender = MailQueueAppender()
+    server = ThreadedHTTPServer(('', 5000), Handler)
+
+    mailq_appender.start()
+    server.serve_forever()
+
+
+
+if __name__ == '__main__':
+    main()
+
+
