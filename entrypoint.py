ddafd4fd9a1ae45dda3160fbd8bdb864a9539829
Vlad Skvortsov <vss@73rus.com>
2017-11-05 09:43:02 -0800

    Make entrypoint modes defined as data

--- entrypoint.py
+++ entrypoint.py
@@ -3,24 +3,26 @@
 import argparse
 import subprocess
 
+MODES = {
+    'frontend': ['/app/frontend.py'],
+    'unittests': ['python3', '-m', 'unittest',
+        'frontend',
+        'mailqueue'
+    ]
+}
+
 def main():
     args = parse_arguments()
 
-    if args.mode == 'frontend':
-        subprocess.run(['/app/frontend.py'], check=True)
-    elif args.mode == 'unittests':
-        modules = [
-            'frontend',
-            'mailqueue'
-        ]
-        subprocess.run(['python3', '-m', 'unittest'] + modules, check=True)
-    else:
+    if args.mode not in MODES:
         raise NotImplementedError
+
+    subprocess.run(MODES[args.mode], check=True)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='MiniMailGun entrypoint')
-    parser.add_argument('mode', help='mode to start in', choices=['frontend', 'unittests'])
+    parser.add_argument('mode', help='mode to start in', choices=sorted(MODES.keys()))
 
     return parser.parse_args()
 
