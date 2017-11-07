a54d03475c44a40efe9bf670a522710c731c9bfb
Vlad Skvortsov <vss@73rus.com>
2017-11-06 16:45:51 -0800

    Add dummy implementation of MailQueue()'s get() and put()

--- mailqueue.py
+++ mailqueue.py
@@ -1,11 +1,24 @@
 import unittest
 
 class MailQueue:
+    def __init__(self):
+        self._queue = []
+
+    def get(self):
+        try:
+            return self._queue.pop()
+        except IndexError:
+            return None
+
     def put(self, message):
-        pass
+        self._queue.append(message)
 
 class TestMailQueue(unittest.TestCase):
-    def test_put(self):
+    def test_roundtrip(self):
+        message = 'something'
         mq = MailQueue()
-        mq.put('something')
-        # TODO: make it a round-trip test
+        mq.put(message)
+        self.assertEqual(message, mq.get())
+
+    def test_empty_queue_should_return_none_on_get(self):
+        self.assertIsNone(MailQueue().get())
