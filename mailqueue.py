d759415bd33678dc1c0a36c95b9febf5b7fdaeaa
Vlad Skvortsov <vss@73rus.com>
2017-11-11 15:14:31 -0800

    Add support for "next scheduled attempt" for an envelope
    MailQueue now has method schedule_retry_in(). The database now selects
    envelopes which are already up for next delivery attempt.

--- mailqueue.py
+++ mailqueue.py
@@ -1,10 +1,9 @@
-import collections
 import email
 import os
 import sqlite3
+import time
 
 
-Envelope = collections.namedtuple('Envelope', 'sender recipients destination_domain message id')
 class Envelope:
     def __init__(self, **kwargs):
         self.id = None
@@ -26,17 +25,28 @@ class MailQueue:
             except FileNotFoundError:
                 pass
 
+        self.clock = time
+
         self._db_conn = sqlite3.connect(self.DB_FILE)
         self._db_conn.row_factory = sqlite3.Row
         self._db_cursor = self._db_conn.cursor()
 
         self._execute_committing(
-            'CREATE TABLE IF NOT EXISTS envelopes '
-            '(sender text, recipients text, destination_domain text, message text)'
+            "CREATE TABLE IF NOT EXISTS envelopes ("
+            "sender TEXT, "
+            "recipients TEXT, "
+            "destination_domain TEXT, "
+            "message TEXT, "
+            "next_attempt_at INTEGER DEFAULT (strftime('%s', CURRENT_TIMESTAMP))"
+            ")"
         )
 
     def get(self):
-        self._db_cursor.execute('SELECT rowid, * FROM envelopes LIMIT 1')
+        self._db_cursor.execute(
+            "SELECT rowid, * FROM envelopes "
+            "WHERE next_attempt_at <= ? LIMIT 1",
+            (self.clock.time(),)
+        )
         row = self._db_cursor.fetchone()
 
         if not row:
@@ -45,10 +55,11 @@ class MailQueue:
         # TODO: test parsing recipients
         return Envelope(sender=row['sender'], recipients=row['recipients'].split(','),
                         destination_domain=row['destination_domain'],
-                        message=email.message_from_string(row['message']), id=row['rowid'])
+                        message=email.message_from_string(row['message']), id=row['rowid'],
+                        next_attempt_at=int(row['next_attempt_at']))
 
     def mark_as_sent(self, envelope):
-        assert envelope.id, '{}: invalid envelope id "{}"'.format(envelope, envelope.id)
+        self._assert_envelope_has_id(envelope)
         self._execute_committing('DELETE FROM envelopes WHERE rowid=?', (envelope.id, ))
 
     def put(self, envelope):
@@ -58,6 +69,17 @@ class MailQueue:
             (envelope.sender, ','.join(envelope.recipients), envelope.destination_domain,
              str(envelope.message))
         )
+
+    def schedule_retry_in(self, envelope, retry_after):
+        self._assert_envelope_has_id(envelope)
+        self._execute_committing(
+            "UPDATE envelopes SET next_attempt_at=? WHERE rowid=?",
+            (envelope.next_attempt_at + retry_after, envelope.id)
+        )
+
+    @staticmethod
+    def _assert_envelope_has_id(envelope):
+        assert envelope.id, '{}: invalid envelope id "{}"'.format(envelope, envelope.id)
 
     def _execute_committing(self, statement, *extra_args):
         self._db_cursor.execute(statement, *extra_args)
--- test_mailqueue.py
+++ test_mailqueue.py
@@ -1,11 +1,23 @@
 from email.message import EmailMessage
+import time
 import unittest
 
 from mailqueue import Envelope, MailQueue
 
 
 class TestMailQueue(unittest.TestCase):
+    class FakeClock:
+        def __init__(self):
+            self._now = time.time()
+
+        def set(self, timestamp):
+            self._now = timestamp
+
+        def time(self):
+            return self._now
+
     def setUp(self):
+        self.mq = MailQueue(fresh=True)
         self.valid_envelope = Envelope(sender='sender@address.com',
                                        recipients=['alice@target.domain', 'bob@target.domain'],
                                        destination_domain='target.domain',
@@ -13,30 +25,49 @@ class TestMailQueue(unittest.TestCase):
                                        )
 
     def test_roundtrip(self):
-        mq = MailQueue(fresh=True)
-        mq.put(self.valid_envelope)
-        self.assertEnvelopesEqual(self.valid_envelope, mq.get())
+        self.mq.put(self.valid_envelope)
+        self.assertEnvelopesEqual(self.valid_envelope, self.mq.get())
 
     def test_empty_queue_should_return_none_on_get(self):
-        self.assertIsNone(MailQueue(fresh=True).get())
+        self.assertIsNone(self.mq.get())
 
     def test_mailqueue_with_fresh_should_drop_database(self):
-        mq = MailQueue(fresh=True)
-        mq.put(self.valid_envelope)
+        self.mq.put(self.valid_envelope)
         self.assertIsNone(MailQueue(fresh=True).get())
 
     def test_messages_should_be_persisted(self):
-        mq = MailQueue(fresh=True)
-        mq.put(self.valid_envelope)
+        self.mq.put(self.valid_envelope)
         self.assertEnvelopesEqual(self.valid_envelope, MailQueue().get())
 
     def test_message_marked_as_sent_should_not_be_retrieved(self):
-        mq = MailQueue(fresh=True)
-        mq.put(self.valid_envelope)
-        item = mq.get()
-        mq.mark_as_sent(item)
+        self.mq.put(self.valid_envelope)
+        item = self.mq.get()
+        self.mq.mark_as_sent(item)
 
-        self.assertIsNone(mq.get())
+        self.assertIsNone(self.mq.get())
+
+    def test_message_to_retry_should_not_be_retrieved(self):
+        self.mq.put(self.valid_envelope)
+        e = self.mq.get()
+        self.mq.schedule_retry_in(e, 1000)
+        self.assertIsNone(self.mq.get())
+
+    def test_message_to_retry_should_be_retrieved_once_its_time_comes(self):
+        self.mq.clock = self.FakeClock()
+        self.mq.put(self.valid_envelope)
+        e = self.mq.get()
+        self.mq.schedule_retry_in(e, 300)
+        self.mq.clock.set(e.next_attempt_at + 600)
+        self.assertEqual(e.id, self.mq.get().id)
+
+    def test_mixed_messages_should_result_in_getting_correct_later_message(self):
+        self.mq.put(self.valid_envelope)
+        self.mq.put(self.valid_envelope)
+        e = self.mq.get()
+        self.mq.schedule_retry_in(e, 1000)
+        self.assertNotEqual(e.id, self.mq.get())
+
+    # TODO increment number of retries
 
     def assertEnvelopesEqual(self, expected, actual):
         self.assertEqual(expected.sender, actual.sender)
