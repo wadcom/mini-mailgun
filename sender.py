c7a23fdd8e10b82b08c768ecbbb0c6062bf6bd60
Vlad Skvortsov <vss@73rus.com>
2017-11-11 10:44:21 -0800

    Factor out (still dirty) SMTP client logic into SMTPClient class

--- sender.py
+++ sender.py
@@ -10,6 +10,8 @@ def main():
     # TODO: this is a prototype and should be reworked
 
     mq = mailqueue.MailQueue()
+    client = SMTPClient()
+
     while True:
         envelope = mq.get()
         if not envelope:
@@ -18,6 +20,14 @@ def main():
 
         smtp_hostname = get_mx_for_domain(envelope.destination_domain)
 
+        client.send(smtp_hostname, envelope)
+
+        # TODO: handle errors
+        mq.mark_as_sent(envelope)
+
+
+class SMTPClient:
+    def send(self, smtp_hostname, envelope):
         try:
             # TODO: take port from configuration
             server = smtplib.SMTP(host=smtp_hostname, port=5000)
@@ -28,11 +38,9 @@ def main():
             # TODO: mark delivery failure
             sys.exit(1)
 
+        # TODO: use correct recipients
         server.send_message(envelope.message)
         server.quit()
-
-        # TODO: handle errors
-        mq.mark_as_sent(envelope)
 
 
 def get_mx_for_domain(domain):
