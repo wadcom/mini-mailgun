6c9e126b462b388f7d8b72d8563d612edd0ba061
Vlad Skvortsov <vss@73rus.com>
2017-11-10 21:01:23 -0800

    Cosmetics: restructure the loop to avoid indentation

--- sender.py
+++ sender.py
@@ -12,26 +12,27 @@ def main():
     mq = mailqueue.MailQueue()
     while True:
         envelope = mq.get()
-        if envelope:
-            smtp_hostname = get_mx_for_domain(envelope.destination_domain)
-
-            try:
-                # TODO: take port from configuration
-                server = smtplib.SMTP(host=smtp_hostname, port=5000)
-            except socket.gaierror:
-                import sys
-                # TODO: log it properly
-                sys.stderr.write('error resolving {}\n'.format(smtp_hostname))
-                # TODO: mark delivery failure
-                sys.exit(1)
-
-            server.send_message(envelope.message)
-            server.quit()
-
-            # TODO: handle errors
-            mq.mark_as_sent(envelope)
-        else:
+        if not envelope:
             time.sleep(1)
+            continue
+
+        smtp_hostname = get_mx_for_domain(envelope.destination_domain)
+
+        try:
+            # TODO: take port from configuration
+            server = smtplib.SMTP(host=smtp_hostname, port=5000)
+        except socket.gaierror:
+            import sys
+            # TODO: log it properly
+            sys.stderr.write('error resolving {}\n'.format(smtp_hostname))
+            # TODO: mark delivery failure
+            sys.exit(1)
+
+        server.send_message(envelope.message)
+        server.quit()
+
+        # TODO: handle errors
+        mq.mark_as_sent(envelope)
 
 
 def get_mx_for_domain(domain):
