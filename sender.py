ff2c947dbbbe17ca261eb9ddc35c1da9ec659d98
Vlad Skvortsov <vss@73rus.com>
2017-11-10 20:49:54 -0800

    Sender now uses destination_domain from envelope to find MXs
    This is still a prototype code, just to see how it falls out.
    Single-threaded, one-message-at-a-time, no error handling. Will clean
    up once the structure emerges.

--- sender.py
+++ sender.py
@@ -1,27 +1,44 @@
 #! /usr/bin/env python3
 
 import smtplib
+import socket
 import time
 
 import mailqueue
-
 
 def main():
     # TODO: this is a prototype and should be reworked
 
     mq = mailqueue.MailQueue()
     while True:
-        q_item = mq.get()
-        if q_item:
-            # TODO: take hostname and port from configuration
-            server = smtplib.SMTP(host='smtpstub', port=5000)
-            server.send_message(q_item.as_email())
+        envelope = mq.get()
+        if envelope:
+            smtp_hostname = get_mx_for_domain(envelope.destination_domain)
+
+            try:
+                # TODO: take port from configuration
+                server = smtplib.SMTP(host=smtp_hostname, port=5000)
+            except socket.gaierror:
+                import sys
+                # TODO: log it properly
+                sys.stderr.write('error resolving {}\n'.format(smtp_hostname))
+                # TODO: mark delivery failure
+                sys.exit(1)
+
+            server.send_message(envelope.message)
             server.quit()
 
             # TODO: handle errors
-            mq.mark_as_sent(q_item)
+            mq.mark_as_sent(envelope)
         else:
             time.sleep(1)
+
+
+def get_mx_for_domain(domain):
+    return {
+        'a.com': 'smtp-a',
+        'b.com': 'smtp-b',
+    }[domain]
 
 
 if __name__ == '__main__':
