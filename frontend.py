61a3882d5da405e337fbfc08b7500afebb41370c
Vlad Skvortsov <vss@73rus.com>
2017-11-12 20:31:16 -0800

    Add /status endpoint
    The /send endpoint now returns submission id.

--- frontend.py
+++ frontend.py
@@ -5,6 +5,7 @@ from email.message import EmailMessage
 import http.server
 import json
 import socketserver
+import uuid
 
 import mailqueue
 
@@ -21,6 +22,8 @@ class Handler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path == '/send':
             self._handle_post_with(SendHandler)
+        elif self.path == '/status':
+            self._handle_post_with(StatusHandler)
         else:
             self.send_error(404, 'Not found')
 
@@ -55,22 +58,24 @@ class SendHandler:
 
     def run(self, request_dict):
         message = self._make_message(request_dict)
-        envelopes = self._make_envelopes(message, request_dict['recipients'])
+        submission_id = uuid.uuid4().hex
+        envelopes = self._make_envelopes(submission_id, message, request_dict['recipients'])
 
         for e in envelopes:
             self._mail_queue.put(e)
 
-        return {'result': 'queued'}
+        return {'result': 'queued', 'submission_id': submission_id}
 
     @staticmethod
-    def _make_envelopes(message, recipients):
+    def _make_envelopes(submission_id, message, recipients):
         domain_to_recipients = collections.defaultdict(list)
         for r in recipients:
             _, domain = r.split('@')
             domain_to_recipients[domain].append(r)
 
         return (mailqueue.Envelope(sender='XXX', recipients=r, destination_domain=d,
-                                   message=message)
+                                   message=message, submission_id=submission_id,
+                                   status=mailqueue.Status.QUEUED)
                 for d, r in domain_to_recipients.items())
 
     @staticmethod
@@ -85,6 +90,23 @@ class SendHandler:
             raise ValueError('Missing request field "{}"'.format(e))
 
         return message
+
+
+class StatusHandler:
+    def __init__(self, mail_queue):
+        self._mail_queue = mail_queue
+
+    def run(self, request_dict):
+        try:
+            submission_id = request_dict['submission_id']
+        except KeyError as e:
+            raise ValueError('Missing request field "{}"'.format(e)) from None
+
+        status = self._mail_queue.get_status(submission_id)
+        if status:
+            return {'result': 'success', 'status': status}
+        else:
+            return {'result': 'error', 'message': 'unknown submission id {}'.format(submission_id)}
 
 
 def main():
--- test_frontend.py
+++ test_frontend.py
@@ -6,6 +6,8 @@ from unittest.mock import MagicMock
 import frontend
 import mailqueue
 
+import testhelpers
+
 
 class TestSendHandler(unittest.TestCase):
     def setUp(self):
@@ -20,12 +22,28 @@ class TestSendHandler(unittest.TestCase):
         }
 
     def test_successful_run_should_return_correct_response(self):
-        self.assertEqual({'result': 'queued'}, self.handler.run(self.valid_request_dict))
+        r = self.handler.run(self.valid_request_dict)
+        self.assertEqual('queued', r['result'])
+        self.assertIn('submission_id', r)
 
     def test_valid_request_should_place_envelope_to_incoming_queue(self):
         e = self._run_handler_and_get_result_envelope(self.valid_request_dict)
         self.assertTrue(isinstance(e, mailqueue.Envelope),
                         msg='expected Envelope, got {}'.format(e.__class__.__name__))
+
+    def test_valid_request_should_assign_submission_id(self):
+        e = self._run_handler_and_get_result_envelope(self.valid_request_dict)
+        self.assertIsNotNone(e.submission_id)
+
+    def test_valid_request_should_be_placed_as_queued(self):
+        e = self._run_handler_and_get_result_envelope(self.valid_request_dict)
+        self.assertEqual(mailqueue.Status.QUEUED, e.status)
+
+    def test_each_request_should_generate_new_submission_id(self):
+        e1 = self._run_handler_and_get_result_envelope(self.valid_request_dict)
+        self.mq.put.reset_mock()
+        e2 = self._run_handler_and_get_result_envelope(self.valid_request_dict)
+        self.assertNotEqual(e1.submission_id, e2.submission_id)
 
     # TODO: test for invalid values of recipients
 
@@ -99,6 +117,41 @@ class TestSendHandler(unittest.TestCase):
         r = dict(self.valid_request_dict)
         r[k] = v
         return r
+
+
+class TestStatusHandler(unittest.TestCase):
+    # TODO: test for missing and invalid parameters in request_dict
+
+    def setUp(self):
+        self.mq = mailqueue.MailQueue(fresh=True)
+        self.handler = frontend.StatusHandler(self.mq)
+
+    def test_unknown_status_should_result_in_error(self):
+        self.mq.get_status = MagicMock(return_value=None)
+        self.assertEqual({'result': 'error', 'message': 'unknown submission id 1'},
+                         self._get_submission_status(1))
+
+    def test_request_should_query_mail_queue(self):
+        status = 'opaque status value from mail queue'
+
+        submission_id = self.mq.put(testhelpers.make_valid_envelope())
+        self.mq.get_status = MagicMock(return_value=status)
+
+        result = self._get_submission_status(submission_id)
+
+        self.mq.get_status.assert_called_once_with(submission_id)
+        self.assertEqual({'result': 'success', 'status': status}, result)
+
+    def test_missing_submission_id_should_raise_value_error(self):
+        self.mq.get_status = MagicMock()
+        with self.assertRaises(ValueError) as cm:
+            self.handler.run({})
+
+        self.assertIn('submission_id', cm.exception.args[0])
+        self.mq.get_status.assert_not_called()
+
+    def _get_submission_status(self, submission_id):
+        return self.handler.run({'submission_id': submission_id})
 
 
 if __name__ == '__main__':
